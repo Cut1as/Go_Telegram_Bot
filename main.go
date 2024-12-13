d7caf2ce47218f4de54611c4d92eb2b04214b02a
Cutlass <senyahozov123@gmail.com>
2024-12-13 16:12:04 +0300

    start

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 
-	botToken := os.Getenv("7611078519:AAFJeJltikMhjsp74ezKMWui7TUn_PqUaV0")
+	botToken := "7611078519:AAFJeJltikMhjsp74ezKMWui7TUn_PqUaV0"
 
 	bot, err := telego.NewBot(botToken, telego.WithDefaultDebugLogger())
 	if err != nil {
